4f97931925e945c97b857140599b701eea3811a9
codebox workspace user <jdimona@gmail.com>
2014-01-14 15:09:40 +0000

    First steps toward a rewrite.

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-app.js
+var static = require('node-static');
+var twitter = require('./lib/twitter/twitter.js');
+
+server = twitter.createServer();
+server.listen(function(stream) {
+  stream.on('data', function(data) {
+    console.log(data);
+  });
+});
+
+var fileServer = new static.Server('./public');
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        fileServer.serve(request, response);
+    }).resume();
+}).listen(3000);
