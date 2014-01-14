f8b9ca4e837fc4ac0acaf382e09d190055501f5c
codebox workspace user <jdimona@gmail.com>
2014-01-14 19:21:51 +0000

    little more...

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-var static = require('node-static');
+var nodeStatic = require('node-static');
 var twitter = require('./lib/twitter/twitter.js');
 
-server = twitter.createServer();
-server.listen(function(stream) {
-  stream.on('data', function(data) {
-    console.log(data);
-  });
+
+var twitterServer = new twitter.Server();
+twitterServer.listen(function(data) {
+  console.log(data);
 });
 
-var fileServer = new static.Server('./public');
 
+var fileServer = new nodeStatic.Server('./public');
 require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-        fileServer.serve(request, response);
-    }).resume();
-}).listen(3000);
+  request.addListener('end', function () {
+    fileServer.serve(request, response);
+  }).resume();
+}).listen(2000);
