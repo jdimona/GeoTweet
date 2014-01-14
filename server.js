8b8e8ac2a3381c739410fa25e95c02fd016d9a36
codebox workspace user <jdimona@gmail.com>
2014-01-14 20:39:53 +0000

    Made reading config file synchronous to avoid ugly code since it has to happen before anything else anyway.

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var nodeStatic = require('node-static');
+var gtUtil = require('./lib/util/util.js');
 var twitter = require('./lib/twitter/twitter.js');
 
+var configFile = './config.json';
+var config = gtUtil.readSyncJSON(configFile);
 
-var twitterServer = new twitter.Server();
+var twitterServer = new twitter.Server(config);
+
 twitterServer.listen(function(data) {
   console.log(data);
 });
-
 
 var fileServer = new nodeStatic.Server('./public');
 require('http').createServer(function (request, response) {
